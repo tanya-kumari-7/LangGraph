347f5b7bfc0f99d77cc26c3048a8b9a892f350fa
Tanya Kumari <87415938+tanya-kumari-7@users.noreply.github.com>
2025-11-10 22:08:29 +0530

    Langgraph_AI_Helper

--- Project_01_AI_Helper.py
+++ Project_01_AI_Helper.py
@@ -11,20 +11,57 @@ api_key = os.getenv("OPENAI_API_KEY")
 
 # Create Model 
 llm_model_chatGPT = ChatOpenAI(model="gpt-4o-mini", api_key=api_key, base_url="https://openrouter.ai/api/v1" )
-print(llm_model_chatGPT.invoke("Hello LangGraph!").content)
+# print(llm_model_chatGPT.invoke("Hello LangGraph!").content)
 
 # Create a graph which will track all what's happing 
 
 class GraphState(TypedDict):
-    questions: str
+    question: str
     answer: str
 graph = StateGraph(GraphState)
 
 # Creating a node : which will decide what to do which input received from users
-
 def decide_node(state):
+    return{}
+
+def decide_node_logic(state):
     q=state["question"]
     if any(ch.isdigit() for ch in q) or any(op in q for op in ["+","-","*","/"]):
         return "math"
     else:
         return "gpt"
+    
+def math_node(state):
+    q=state["question"]
+    try:
+        result = str(eval(q))
+    except Exception:
+        result = "Sorry, I couldn't calculate"
+    return {"answer":result}
+
+def gpt_node(state):
+    q=state["question"]
+    response = llm_model_chatGPT.invoke(q)
+    return {"answer":response.content}
+
+# Add Nodes
+
+graph.add_node("decide",decide_node)
+graph.add_node("math",math_node)
+graph.add_node("gpt",gpt_node)
+
+# Add Edge
+graph.add_edge(START,"decide")
+graph.add_conditional_edges(
+    "decide",
+    decide_node_logic,
+    {"math":"math","gpt":"gpt"}
+)
+graph.add_edge("math",END)
+graph.add_edge("gpt",END)
+
+# compile
+app = graph.compile()
+
+print(app.invoke({"question": "2+3"}))
+print(app.invoke({"question": "Who are you?"}))
