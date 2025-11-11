e5bbdc966a1d0fcba454c9ab682f4328cf788172
Tanya Kumari <87415938+tanya-kumari-7@users.noreply.github.com>
2025-11-11 18:45:34 +0530

    Langgraph_AI_Helper

---
+++ Project_02_Chatbot.py
@@ -0,0 +1,12 @@
+'''
+Project: “Simple Chat Workflow”
+
+You’ll build a tiny chatbot workflow using LangGraph.
+It will:
+
+Take a user’s question as input
+Use a model to generate a response
+Optionally check if the response is polite before replying
+'''
+
+# Imports 
