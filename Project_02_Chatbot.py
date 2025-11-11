8540e08e62dda5886babe4fcef834802cd17e6f0
Tanya Kumari <87415938+tanya-kumari-7@users.noreply.github.com>
2025-11-11 20:14:58 +0530

    Langgraph_Chatbot

--- Project_02_Chatbot.py
+++ Project_02_Chatbot.py
@@ -25,8 +25,8 @@ model_url = os.getenv("base_url")
 # define memory of your workflow [STATE]
 
 class chatstate(TypedDict):
-    question = str
-    response = str
+    question : str
+    response : str
 
 # Craeteing model 
 llm_model = ChatOpenAI(
@@ -41,5 +41,41 @@ def ask_question_node(state:chatstate):
     state['response'] = response.content
     return state
 
+def check_politeness_node(state:chatstate):
+    response = state["response"]
+    if "please" in response.lower() or "thank" in response.lower():
+        print("Response is polite")
+    else:
+        print("Response is not polite")
+
+def end_conversion_node(state:chatstate):
+    print(f"Final response is :{state['response']}")
+    return state
+
+# Building Graph 
+
+graph = StateGraph(chatstate)
+graph.add_node("ask",ask_question_node)
+graph.add_node('check',check_politeness_node)
+graph.add_node('end',end_conversion_node)
 
 
+# define Edge :
+
+graph.add_edge(START,'ask')
+graph.add_edge('ask','check')
+graph.add_edge('check','end')
+graph.add_edge('end',END)
+
+# compliations
+
+app =graph.compile()
+
+# run graph:
+
+if __name__ == "__main__":
+    user_question = input("Enter your question: ASK ANYTHING")
+    initial_question = {"question":user_question,"response":""}
+    final_state = app.invoke(initial_question)
+
+        
