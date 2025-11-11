e292fb8ef9394f5373494199235449cffbb8e44c
Tanya Kumari <87415938+tanya-kumari-7@users.noreply.github.com>
2025-11-11 18:57:21 +0530

    Langgraph_Chatbot

--- Project_02_Chatbot.py
+++ Project_02_Chatbot.py
@@ -11,10 +11,23 @@ Optionally check if the response is polite before replying
 
 # Imports 
 
-from langgraph.graph import StateGraph , START , END
-from langchain_openai import ChatOpenAI
-from typing import TypedDict
-from dotenv import load_dotenv
-import os
+from langgraph.graph import StateGraph , START , END  #[for langgraph edge and state]
+from langchain_openai import ChatOpenAI #[for AI model]
+from typing import TypedDict #[to pass dict in format inside STATE]
+from dotenv import load_dotenv #[call api key from .env file]
+import os #[get key]
+
+# load API Key
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+# define memory of your workflow [STATE]
+
+class chatstate(TypedDict):
+    question = str
+    response = str
+
+# Craeteing model 
+
 
 
