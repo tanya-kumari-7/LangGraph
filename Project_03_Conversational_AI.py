10c7307bd49a8d7855216f08ad4f34e682e0f1ce
Tanya Kumari <87415938+tanya-kumari-7@users.noreply.github.com>
2025-11-12 20:57:35 +0530

    project_3_Langgraph_Conversational_AI

--- Project_03_Conversational_AI.py
+++ Project_03_Conversational_AI.py
@@ -28,3 +28,19 @@ load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 base_url = os.getenv("base_url")
 
+#  Define state
+
+class ChatState(TypedDict):
+    name : str
+    age : int
+    location : str
+    mobile : int
+    issue : str
+
+#  Createing LLM Model 
+
+llm_model = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    api_key=api_key,
+    base_url=base_url
+)
