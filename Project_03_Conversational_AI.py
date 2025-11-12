4e11fb861316313c98ec68467a0c2751267b08ff
Tanya Kumari <87415938+tanya-kumari-7@users.noreply.github.com>
2025-11-12 20:32:04 +0530

    project_3_Langgraph_Conversational_AI

---
+++ Project_03_Conversational_AI.py
@@ -0,0 +1,14 @@
+
+"""
+Description of the project :
+Medical AI assistant
+
+1. GET data from user : 
+     a. Name of the user
+     b. Age of the user
+     c. Location of the user
+     d. Mobile_no of the user
+     e. Medical Issue description
+"""
+
+
